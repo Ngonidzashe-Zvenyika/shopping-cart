62f66722062751b52ad88f2bf92ff62958ad47f5
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2024-07-04 11:13:50 +0200

    Add application component with predetermined structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Movies from "./components/Movies";
+import TvShows from "./components/TvShows";
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
+import Footer from "./components/Footer";
+import ErrorPage from "./components/ErrorPage";
 import "./App.css";
 
 function App() {
-  return <h1>This is the main app</h1>;
+  const { target } = useParams();
+  const [cart, setCart] = useState([]);
+  const [modal, setModal] = useState(null);
+  const [searchItem, setSearchItem] = useState("");
+  const [menu, setMenu] = useState(false);
+  return (
+    <>
+      <Header
+        cartLength={cart.length}
+        menu={menu}
+        setMenu={setMenu}
+        setSearchItem={setSearchItem}
+        setModal={setModal}
+      />
+      {target === undefined ? (
+        <Home
+          searchItem={searchItem}
+          setSearchItem={setSearchItem}
+          setModal={setModal}
+          setMenu={setMenu}
+        />
+      ) : target === "movies" ? (
+        <Movies setMenu={setMenu} setModal={setModal} />
+      ) : target === "tv-shows" ? (
+        <TvShows setMenu={setMenu} setModal={setModal} />
+      ) : target === "cart" ? (
+        <Cart cart={cart} setCart={setCart} />
+      ) : (
+        <ErrorPage />
+      )}
+      {modal && (
+        <Modal
+          modal={modal}
+          setModal={setModal}
+          cart={cart}
+          setCart={setCart}
+        />
+      )}
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
